be35ed6f06a22c3640adc3dfe1649ec319000312
Christian Johansson <christian@cvj.se>
2018-12-17 12:33:32 +0100

    Added sample files for inline and alternative control structures

---
+++ sample-php-files/alternative-control-structure.php
@@ -0,0 +1,22 @@
+<?php
+
+if (true):
+    echo 'was here';
+    echo 'was here 2';
+endif;
+
+if (true):
+    echo 'was here';
+else:
+    echo 'was here 2';
+endif;
+
+
+switch (true):
+    case true:
+        echo 'was here 1';
+        echo 'was here 2';
+    default:
+        echo 'the default';
+endswitch;
+
---
+++ sample-php-files/inline-control-structures.php
@@ -0,0 +1,17 @@
+<?php
+
+if (true)
+    echo 'was here';
+echo 'was here 2';
+
+if (true)
+    echo 'was here';
+else
+    echo 'was here 2';
+
+switch (true) {
+    case true:
+        echo 'something here';
+    default:
+        echo 'the default here';
+}
