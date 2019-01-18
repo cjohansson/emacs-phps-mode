caafeba7a46c1a97907ddef92cba032d714f822f
Christian Johansson <christian@cvj.se>
2019-01-18 10:05:35 +0100

    Changed idle interval for incremental lexer

--- phps-mode.el
+++ phps-mode.el
@@ -59,7 +59,7 @@
 (defvar phps-mode-use-psr-2 t
   "Whether to use PSR-2 guidelines for white-space or not.")
 
-(defvar phps-mode-idle-interval 0.2
+(defvar phps-mode-idle-interval 0.8
   "Idle seconds before running incremental lexer.")
 
 (define-derived-mode phps-mode prog-mode "PHPs"
