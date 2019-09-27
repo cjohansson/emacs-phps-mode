62468a25c794bd82c02e9a5acba684f8d0e68f02
Christian Johansson <christian@cvj.se>
2019-09-27 08:25:50 +0200

    (uncomment-region) now only un-comments commented PHP code

--- phps-mode-test-functions.el
+++ phps-mode-test-functions.el
@@ -959,11 +959,11 @@
      (should (equal buffer-contents "<?php\nnamespace myNamespace;\nclass myClass extends myAbstract /* implements myInterface */ {\n    public function myFunctionA($myArg = null) {}\n    protected function myFunctionB($myArg = 'abc') {}\n}\n"))))
 
   (phps-mode-test-with-buffer
-   "// <?php\n// namespace myNamespace;\n// class myClass extends myAbstract implements myInterface {\n//     public function myFunctionA($myArg = null) {}\n//     protected function myFunctionB($myArg = 'abc') {}\n// }\n"
+   "// <?php\n// namespace myNamespace;\n// class myClass extends myAbstract implements myInterface {\n//    public function myFunctionA($myArg = null) {}\n//    protected function myFunctionB($myArg = 'abc') {}\n//}\n"
    "Uncomment object-oriented file with bracket-less namespace, class that extends and implements and functions with optional arguments"
    (uncomment-region (point-min) (point-max))
    (let ((buffer-contents (buffer-substring-no-properties (point-min) (point-max))))
-     (should (equal buffer-contents "<?php\nnamespace myNamespace;\nclass myClass extends myAbstract implements myInterface {\n    public function myFunctionA($myArg = null) {}\n    protected function myFunctionB($myArg = 'abc') {}\n}\n"))))
+     (should (equal buffer-contents "// <?php\n namespace myNamespace;\n class myClass extends myAbstract implements myInterface {\n    public function myFunctionA($myArg = null) {}\n    protected function myFunctionB($myArg = 'abc') {}\n}\n"))))
 
   (phps-mode-test-with-buffer
    "<?php\nnamespace myNamespace;\nclass myClass extends myAbstract/*  implements myInterface  */{\n    public function myFunctionA($myArg = null) {}\n    protected function myFunctionB($myArg = 'abc') {}\n}\n"
