e039f46440ab2493c2b59da82d97789182963205
Christian Johansson <christian@cvj.se>
2019-11-22 17:06:09 +0100

    Added todo item

--- test/phps-mode-test-functions.el
+++ test/phps-mode-test-functions.el
@@ -142,6 +142,8 @@
               buffer-contents
               "<?php\nif ($test) {\n    \n} else if ($test) {\n    \n}\n"))))
 
+  ;; TODO Should support indentation line after } else ($test) {
+
   )
 
 (defun phps-mode-test-functions-move-lines-indent ()
