6b5de4c78ad577a8709031e0e308151f5cc1e07c
Christian Johansson <christian@cvj.se>
2020-01-16 12:00:38 +0100

    Using :debug display-warnings because they are thread-safe

--- phps-mode-analyzer.el
+++ phps-mode-analyzer.el
@@ -231,7 +231,7 @@
                          (message "Asynchronous serial command using async.el finished, elapsed: %fs" elapsed)))
 
                       (when (string= status "error")
-                        (display-warning 'phps-mode (format "Async error %s" (cdr start-return))))
+                        (display-warning 'phps-mode (format "Async error %s" (cdr start-return)) :debug))
 
                       (when (and (boundp 'callback)
                                  callback)
@@ -1959,7 +1959,7 @@
                 (error-start)
                 (error-end))
             (when errors
-              (display-warning 'phps-mode (format "Lex Errors: %s" (car errors)))
+              (display-warning 'phps-mode (format "Lex Errors: %s" (car errors)) :debug)
               (setq error-start (car (cdr errors)))
               (when error-start
                 (if (car (cdr (cdr errors)))
@@ -2267,7 +2267,7 @@
              (error-start)
              (error-end))
          (when errors
-           (display-warning 'phps-mode (format "Incremental Lex Errors: %s" (car errors)))
+           (display-warning 'phps-mode (format "Incremental Lex Errors: %s" (car errors)) :debug)
            (setq error-start (car (cdr errors)))
            (when error-start
              (if (car (cdr (cdr errors)))
