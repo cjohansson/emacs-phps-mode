5774a7a18b38cce49970431357aff5ae61c1c244
Christian Johansson <christian@cvj.se>
2020-04-10 08:59:10 +0200

    Fixed issue with buffer focus while lexer was running

--- phps-mode.el
+++ phps-mode.el
@@ -5,8 +5,8 @@
 ;; Author: Christian Johansson <christian@cvj.se>
 ;; Maintainer: Christian Johansson <christian@cvj.se>
 ;; Created: 3 Mar 2018
-;; Modified: 26 Feb 2020
-;; Version: 0.3.38
+;; Modified: 10 Apr 2020
+;; Version: 0.3.39
 ;; Keywords: tools, convenience
 ;; URL: https://github.com/cjohansson/emacs-phps-mode
 
