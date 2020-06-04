d583009a52dec618facb75503779757825b307a7
Christian Johansson <christian@cvj.se>
2020-06-04 15:30:13 +0200

    Added parser-block namespace-name

--- phps-mode-wy.el
+++ phps-mode-wy.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2018-2020  Free Software Foundation, Inc.
 
 ;; Author: Christian Johansson <christianjohansson@Christians-MacBook-Air.local>
-;; Created: 2020-06-04 15:29:21+0200
+;; Created: 2020-06-04 15:30:03+0200
 ;; Keywords: syntax
 ;; X-RCS: $Id$
 
@@ -287,6 +287,11 @@
         (left 43 45)
         (left 42 47 37 %precedence 33 %precedence T_INSTANCEOF %precedence 126 T_INT_CAST T_DOUBLE_CAST T_STRING_CAST T_ARRAY_CAST T_OBJECT_CAST T_BOOL_CAST T_UNSET_CAST 64)
         (right T_POW %precedence T_CLONE %precedence T_NOELSE %precedence T_ELSEIF %precedence T_ELSE))
+       (namespace_name
+        ((T_STRING)
+         ($1))
+        ((namespace_name T_NS_SEPARATOR T_STRING)
+         (ZEND_AST_APPEND_STR $1 $3)))
        (name
         ((namespace_name)
          (progn
