2c399de406963492bbd2246100c6370d9591992a
Christian Johansson <christian@cvj.se>
2020-07-02 09:10:46 +0200

    Fixed lexer-based indentation issue with switch, case with trailing comments

--- phps-mode.el
+++ phps-mode.el
@@ -5,8 +5,8 @@
 ;; Author: Christian Johansson <christian@cvj.se>
 ;; Maintainer: Christian Johansson <christian@cvj.se>
 ;; Created: 3 Mar 2018
-;; Modified: 25 May 2020
-;; Version: 0.3.51
+;; Modified: 7 Jul 2020
+;; Version: 0.3.52
 ;; Keywords: tools, convenience
 ;; URL: https://github.com/cjohansson/emacs-phps-mode
 
