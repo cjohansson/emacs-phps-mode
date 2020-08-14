3cbb92ac33975909a2dacfa551d2c2069b3ba588
Christian Johansson <christian@cvj.se>
2020-08-14 11:52:26 +0200

    Added more grammar

--- phps-mode-parser-custom-grammar.el
+++ phps-mode-parser-custom-grammar.el
@@ -65,20 +65,99 @@
    (lambda(a) a)))
  phps-mode-parser-custom-grammar)
 
-;; top_statement_list
+;; reserved_non_modifiers:
 (puthash
- 'top_statement_list
+ 'reserved_non_modifiers
  (list
-  (list
-   (list 'top_statement 'top_statement_list)
-   (lambda(a b) (list a b)))
-  (list
-   (list 'empty)
-   (lambda() (list 'phps-mode-parser--ZEND_AST_STMT_LIST))))
+  (list (list 'T_INCLUDE) (lambda(a) a))
+  (list (list 'T_INCLUDE_ONCE) (lambda(a) a))
+  (list (list 'T_EVAL) (lambda(a) a))
+  (list (list 'T_REQUIRE) (lambda(a) a))
+  (list (list 'T_REQUIRE_ONCE) (lambda(a) a))
+  (list (list 'T_LOGICAL_OR) (lambda(a) a))
+  (list (list 'T_LOGICAL_XOR) (lambda(a) a))
+  (list (list 'T_LOGICAL_AND) (lambda(a) a))
+  (list (list 'T_INSTANCEOF) (lambda(a) a))
+  (list (list 'T_NEW) (lambda(a) a))
+  (list (list 'T_CLONE) (lambda(a) a))
+  (list (list 'T_EXIT) (lambda(a) a))
+  (list (list 'T_IF) (lambda(a) a))
+  (list (list 'T_ELSEIF) (lambda(a) a))
+  (list (list 'T_ELSE) (lambda(a) a))
+  (list (list 'T_ENDIF) (lambda(a) a))
+  (list (list 'T_ECHO) (lambda(a) a))
+  (list (list 'T_DO) (lambda(a) a))
+  (list (list 'T_WHILE) (lambda(a) a))
+  (list (list 'T_ENDWHILE) (lambda(a) a))
+  (list (list 'T_FOR) (lambda(a) a))
+  (list (list 'T_ENDFOR) (lambda(a) a))
+  (list (list 'T_FOREACH) (lambda(a) a))
+  (list (list 'T_ENDFOREACH) (lambda(a) a))
+  (list (list 'T_DECLARE) (lambda(a) a))
+  (list (list 'T_ENDDECLARE) (lambda(a) a))
+  (list (list 'T_AS) (lambda(a) a))
+  (list (list 'T_TRY) (lambda(a) a))
+  (list (list 'T_CATCH) (lambda(a) a))
+  (list (list 'T_FINALLY) (lambda(a) a))
+  (list (list 'T_THROW) (lambda(a) a))
+  (list (list 'T_USE) (lambda(a) a))
+  (list (list 'T_INSTEADOF) (lambda(a) a))
+  (list (list 'T_GLOBAL) (lambda(a) a))
+  (list (list 'T_VAR) (lambda(a) a))
+  (list (list 'T_UNSET) (lambda(a) a))
+  (list (list 'T_ISSET) (lambda(a) a))
+  (list (list 'T_EMPTY) (lambda(a) a))
+  (list (list 'T_CONTINUE) (lambda(a) a))
+  (list (list 'T_GOTO) (lambda(a) a))
+  (list (list 'T_FUNCTION) (lambda(a) a))
+  (list (list 'T_CONST) (lambda(a) a))
+  (list (list 'T_RETURN) (lambda(a) a))
+  (list (list 'T_PRINT) (lambda(a) a))
+  (list (list 'T_YIELD) (lambda(a) a))
+  (list (list 'T_LIST) (lambda(a) a))
+  (list (list 'T_SWITCH) (lambda(a) a))
+  (list (list 'T_ENDSWITCH) (lambda(a) a))
+  (list (list 'T_CASE) (lambda(a) a))
+  (list (list 'T_DEFAULT) (lambda(a) a))
+  (list (list 'T_BREAK) (lambda(a) a))
+  (list (list 'T_ARRAY) (lambda(a) a))
+  (list (list 'T_CALLABLE) (lambda(a) a))
+  (list (list 'T_EXTENDS) (lambda(a) a))
+  (list (list 'T_IMPLEMENTS) (lambda(a) a))
+  (list (list 'T_NAMESPACE) (lambda(a) a))
+  (list (list 'T_TRAIT) (lambda(a) a))
+  (list (list 'T_INTERFACE) (lambda(a) a))
+  (list (list 'T_CLASS) (lambda(a) a))
+  (list (list 'T_CLASS_C) (lambda(a) a))
+  (list (list 'T_TRAIT_C) (lambda(a) a))
+  (list (list 'T_FUNC_C) (lambda(a) a))
+  (list (list 'T_METHOD_C) (lambda(a) a))
+  (list (list 'T_LINE) (lambda(a) a))
+  (list (list 'T_FILE) (lambda(a) a))
+  (list (list 'T_DIR) (lambda(a) a))
+  (list (list 'T_NS_C) (lambda(a) a))
+  (list (list 'T_FN) (lambda(a) a))
+  )
+ phps-mode-parser-custom-grammar)
+
+;; semi_reserved:
+(puthash
+ 'semi_reserved
+ (list
+  (list (list 'reserved_non_modifiers) (lambda(a) a))
+  (list (list 'T_STATIC) (lambda(a) a))
+  (list (list 'T_ABSTRACT) (lambda(a) a))
+  (list (list 'T_FINAL) (lambda(a) a))
+  (list (list 'T_PRIVATE) (lambda(a) a))
+  (list (list 'T_PROTECTED) (lambda(a) a))
+  (list (list 'T_PUBLIC) (lambda(a) a))
+  )
  phps-mode-parser-custom-grammar)
 
 ;; TODO Was here
-;; top_statement
+
+
+;; top_statement:
 (puthash
  'top_statement
  (list
@@ -110,6 +189,18 @@
   (list
    (list 'T_NAMESPACE "{" 'top_statement_list "}")
    (lambda(_a _b c _d) (list 'phps-mode-parser--ZEND_AST_NAMESPACE c)))
+  (list
+   (list 'T_USE 'mixed_group_use_declaration ";")
+   (lambda(_a b _c) b))
+  (list
+   (list 'T_USE 'use_type 'group_use_declaration ";")
+   (lambda(_a b c _d) (list 'attr b c)))
+  (list
+   (list 'T_USE 'use_declarations ";")
+   (lambda(_a b _c) (list 'attr b 'phps-mode-parser--ZEND_SYMBOL_CLASS)))
+  (list
+   (list 'T_CONST 'const_list ";")
+   (lambda(_a b _c) b))
   )
  phps-mode-parser-custom-grammar)
 
