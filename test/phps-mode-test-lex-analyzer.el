54caee5e6093d1c43d4d02c4b317a483973e2956
Christian Johansson <christian@cvj.se>
2020-10-16 12:52:19 +0200

    Added failing test for issue with commenting while changes has not been processed

--- test/phps-mode-test-lex-analyzer.el
+++ test/phps-mode-test-lex-analyzer.el
@@ -1309,6 +1309,15 @@
    (let ((buffer-contents (buffer-substring-no-properties (point-min) (point-max))))
      (should (equal buffer-contents "<?php\n$var = '123';\n$var = 'abc';\n"))))
 
+  (phps-mode-test--with-buffer
+   "<?php\n$var1 = '123';"
+   "Comment region after changes has been made to buffer"
+   (goto-char 19)
+   (insert " def")
+   (comment-region 7 (point-max))
+   (let ((buffer-contents (buffer-substring-no-properties (point-min) (point-max))))
+     (should (equal buffer-contents "<?php\n$var1 = '123 def';\n"))))
+
   )
 
 (defun phps-mode-test-lex-analyzer--get-inline-html-indentation ()
