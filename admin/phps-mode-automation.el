78f91a1defb4640a11d37733f09704fe86d69a74
Christian Johansson <christian@cvj.se>
2021-03-11 08:57:43 +0100

    Started on generated parser via emacs-parser-generator

--- admin/phps-mode-automation.el
+++ admin/phps-mode-automation.el
@@ -1,4 +1,4 @@
-;;; phps-automation --- Generate a Wisent Parser file -*- lexical-binding: t -*-
+;;; phps-mode-automation --- Generate a Wisent Parser file -*- lexical-binding: t -*-
 
 ;; Copyright (C) 2018-2021  Free Software Foundation, Inc.
 
@@ -27,41 +27,8 @@
 
 ;;; Code:
 
-(let ((php-yacc-url "https://raw.githubusercontent.com/php/php-src/master/Zend/zend_language_parser.y")
-      (php-yacc-file (expand-file-name "zend_language_parser.y"))
-      (wisent-destination (expand-file-name "../phps-mode-parser-grammar-raw.wy"))
-      (header (expand-file-name "phps-mode-automation-header.wy"))
-      (terminal-replacements (make-hash-table :test 'equal)))
-
-  (puthash "'+'" "ADDITION" terminal-replacements)
-  (puthash "'='" "ASSIGN" terminal-replacements)
-  (puthash "'@'" "AT" terminal-replacements)
-  (puthash "'`'" "BACKTICK" terminal-replacements)
-  (puthash "'&'" "BITWISE_AND" terminal-replacements)
-  (puthash "'|'" "BITWISE_OR" terminal-replacements)
-  (puthash "'}'" "CLOSE_CURLY_BRACKET" terminal-replacements)
-  (puthash "')'" "CLOSE_PARENTHESIS" terminal-replacements)
-  (puthash "']'" "CLOSE_SQUARE_BRACKET" terminal-replacements)
-  (puthash "':'" "COLON" terminal-replacements)
-  (puthash "','" "COMMA" terminal-replacements)
-  (puthash "'$'" "DOLLAR_SIGN" terminal-replacements)
-  (puthash "'\"'" "DOUBLE_QUOTE" terminal-replacements)
-  (puthash "'/'" "DIVISION" terminal-replacements)
-  (puthash "'.'" "DOT" terminal-replacements)
-  (puthash "'>'" "GREATER_THAN" terminal-replacements)
-  (puthash "'<'" "LESSER_THAN" terminal-replacements)
-  (puthash "'%'" "MODULO" terminal-replacements)
-  (puthash "'*'" "MULTIPLICATION" terminal-replacements)
-  (puthash "'!'" "NEGATION" terminal-replacements)
-  (puthash "'{'" "OPEN_CURLY_BRACKET" terminal-replacements)
-  (puthash "'('" "OPEN_PARENTHESIS" terminal-replacements)
-  (puthash "'['" "OPEN_SQUARE_BRACKET" terminal-replacements)
-  (puthash "'^'" "POW" terminal-replacements)
-  (puthash "'?'" "QUESTION_MARK" terminal-replacements)
-  (puthash "';'" "SEMICOLON" terminal-replacements)
-  (puthash "'''" "SINGLE_QUOTE" terminal-replacements)
-  (puthash "'-'" "SUBTRACTION" terminal-replacements)
-  (puthash "'~'" "UNARY" terminal-replacements)
+(let ((php-yacc-url "https://raw.githubusercontent.com/php/php-src/php-8.0.0/Zend/zend_language_parser.y")
+      (php-yacc-file (expand-file-name "zend_language_parser.y")))
 
   ;; Download Yacc if not available
   (unless (file-exists-p php-yacc-file)
@@ -69,18 +36,6 @@
     (url-copy-file php-yacc-url php-yacc-file t t)
     (message "Download completed"))
 
-  ;; Generate grammar
-  (message "Generating Wisent grammar..")
-  (if (fboundp 'emacs-wisent-grammar-converter--generate-grammar-from-filename)
-      (emacs-wisent-grammar-converter--generate-grammar-from-filename
-       php-yacc-file
-       wisent-destination
-       header
-       "phps-mode-parser--"
-       terminal-replacements)
-    (display-warning
-     'warning
-     "Missing emacs-wisent-grammar-converter!"))
   (message "Automation completed"))
 
 (provide 'phps-mode-automation)
