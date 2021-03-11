edf3a8cb262678efe521d7344e39e327dc9fe6b7
Christian Johansson <christian@cvj.se>
2021-03-11 08:57:43 +0100

    Updated version and modified date

--- phps-mode.el
+++ phps-mode.el
@@ -5,8 +5,8 @@
 ;; Author: Christian Johansson <christian@cvj.se>
 ;; Maintainer: Christian Johansson <christian@cvj.se>
 ;; Created: 3 Mar 2018
-;; Modified: 28 Jan 2021
-;; Version: 0.4.0
+;; Modified: 29 Jan 2021
+;; Version: 0.4.1
 ;; Keywords: tools, convenience
 ;; URL: https://github.com/cjohansson/emacs-phps-mode
 
